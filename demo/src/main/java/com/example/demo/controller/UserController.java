bbf7a34c03787c5126695786fc71dcabd1e5a3ac
nguyenanhtu002 <tumtp29062k2@gmail.com>
2023-07-31 16:37:03 +0700

    update

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -11,12 +11,15 @@ import com.example.demo.util.TokenUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
+  private static final Logger log = LoggerFactory.getLogger(UserController.class);
   private final UserService userService;
 
   public UserController(UserService userService) {
@@ -26,6 +29,7 @@ public class UserController {
   @PostMapping("")
   public ResponseGeneral<UserResponse> create(
         @RequestBody @Valid UserRequest userRequest) {
+    log.info("(create) request: {}", userRequest);
     UserResponse userResponse = userService.create(userRequest);
     return new ResponseGeneral<>(MessageResponse.CREATE_USER, userResponse, HttpStatus.CREATED.value());
   }
@@ -33,6 +37,7 @@ public class UserController {
   @PutMapping("/id")
   public ResponseGeneral<UserResponse> update(
         @RequestBody @Valid UserRequest userRequest, @PathVariable(name = "id") int id) {
+    log.info("(update) Request to update user with id {}: {}", id, userRequest);
     UserResponse user = userService.update(userRequest, id);
     return new ResponseGeneral<>(MessageResponse.UPDATE_SUCCESS, user, HttpStatus.OK.value());
   }
@@ -40,13 +45,17 @@ public class UserController {
 
   @GetMapping("/")
   public ResponseGeneral<List<UserResponse>> list() {
+    log.info("(list) Request to get all users.");
     List<UserResponse> users = userService.list();
+    log.info("(list) Retrieved {} users: {}", users.size(), users);
     return new ResponseGeneral<>(MessageResponse.GET_USER, users, HttpStatus.OK.value());
   }
 
   @DeleteMapping("/id")
   public ResponseGeneral<Void> delete(@PathVariable(name = "id") int id) {
+    log.info("(delete) Request to delete user with id: {}", id);
     userService.delete(id);
+    log.info("(delete) User with id {} deleted successfully.", id);
     return new ResponseGeneral<>(MessageResponse.DELETE_USER, HttpStatus.OK.value());
   }
 
@@ -55,6 +64,7 @@ public class UserController {
     LoginResponse user = userService.login(loginRequest);
     String token = TokenUtil.generateToken();
     user.setToken(token);
+    log.info("(login) User logged in successfully: {}", user.getUsername());
     return new ResponseGeneral<>(MessageResponse.LOGIN_SUCCESS, user, HttpStatus.OK.value());
   }
 }
